3d3e76bb8db2eee9500f4634bf80c422565ceeb7
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-09-05 18:26:19 -0300

    Adiciona arquivos

---
+++ soma.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int soma(int a) {
+    return 2 + a;
+}
+
+int main() {
+    int valor;
+    cin >> valor;
+    cout << soma(valor) << endl;
+    return 0;
+}
