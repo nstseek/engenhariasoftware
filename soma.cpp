30fca8de7e1bc214e19a5710cc751f66a55d8e80
EMANUEL NOGUEIRA DE BARROS <16204013@pucrs.br>
2019-09-05 18:38:17 -0300

    a commit

--- soma.cpp
+++ soma.cpp
@@ -2,13 +2,15 @@
 
 using namespace std;
 
-int soma(int a) {
-    return 2 + a;
+int soma(int a, int b) {
+    return a + b;
 }
 
 int main() {
     int valor;
+	int valor2
     cin >> valor;
+	cin >> valor2;
     cout << soma(valor) << endl;
     return 0;
 }
